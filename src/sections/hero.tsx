d85e4218ccfedb68698d9fad5645125ec60ec0cc
Braimah-Abiola <braimahabiola.official@gmail.com>
2025-12-04 15:05:27 +0000

    Final push

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -5,24 +5,23 @@ import AppStoreButton from "@/components/landing/app-store-button";
 import GooglePlayButton from "@/components/landing/google-play-button";
 import { LineShadowText } from "@/components/ui/line-shadow-text";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 
 const Hero = () => {
-    const [mobileOs, setMobileOs] = useState<"ios" | "android" | "unknown">(
-        "unknown"
-    );
+    // const [mobileOs, setMobileOs] = useState<"ios" | "android" | "unknown">(
+    //     "unknown"
+    // );
 
-    useEffect(() => {
-        const userAgent = navigator.userAgent || navigator.vendor || (window as any).opera;
+    // useEffect(() => {
+    //     const userAgent = navigator.userAgent || navigator.vendor || (window as any).opera;
 
-        if (/android/i.test(userAgent)) {
-            setMobileOs("android");
-        } else if (/iPad|iPhone|iPod/.test(userAgent) && !(window as any).MSStream) {
-            setMobileOs("ios");
-        } else {
-            setMobileOs("unknown");
-        }
-    }, []);
+    //     if (/android/i.test(userAgent)) {
+    //         setMobileOs("android");
+    //     } else if (/iPad|iPhone|iPod/.test(userAgent) && !(window as any).MSStream) {
+    //         setMobileOs("ios");
+    //     } else {
+    //         setMobileOs("unknown");
+    //     }
+    // }, []);
     return (
         <section className="w-full pt-20">
             <div className=" max-w-6xl mx-auto w-full flex flex-col items-center z-50 relative">
@@ -43,13 +42,9 @@ const Hero = () => {
                 <p className=" text-sm md:text-base text-balance text-muted-foreground leading-tight text-center max-w-xl mt-2">
                     Upload your bet slips, get instant AI analysis, and access expert match predictions. Powered by real-time data and advanced ML and analytics.
                 </p>
-                <div className="flex flex-row gap-3 mt-4 md:mt-6">
-                    {(mobileOs === "ios" || mobileOs === "unknown") && (
-                        <AppStoreButton />
-                    )}
-                    {(mobileOs === "android" || mobileOs === "unknown") && (
-                        <GooglePlayButton />
-                    )}
+                <div className="flex items-center gap-3 mt-4 md:mt-6">
+                    <AppStoreButton />
+                    <GooglePlayButton />
                 </div>
             </div>
         </section>
